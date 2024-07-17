beb098a85c8142395c9925e58a86f900fa7c8dfb
Sivakov Vladislav <svv@salesap.ru>
2024-07-17 15:25:21 +0300

    upd

--- components/TagNews/index.vue
+++ components/TagNews/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="flex gap-[8px] text-[14px] leading-[18px] tracking-[2%]">
-    <nuxt-link v-for="(tag, index) in tags" :to="tag.link" :class="`flex items-center gap-[4px] rounded-md border px-[12px] py-[4px] text-${getColor(index)} border-${getColor(index)}`">
-      <span v-if="tag.before" :class="'w-[11px] h-[11px] rounded-full text-white text-[8px] flex items-center justify-center bg-'+getColor(index) ">{{ tag.before }}</span>
+    <nuxt-link v-for="(tag, index) in tags" :to="tag.link"
+      :class="`flex items-center gap-[4px] rounded-md border px-[12px] py-[4px] text-${getColor(index)} border-${getColor(index)}`">
+      <span v-if="tag.before"
+        :class="'w-[11px] h-[11px] rounded-full text-white text-[8px] flex items-center justify-center bg-' + getColor(index)">{{
+          tag.before }}</span>
       {{ tag.name }}
     </nuxt-link>
   </div>
